359abfb8d8fa0dd219f46a0697719a6281089799
fdlfernandovillanueva <141683660+fdlfernandovillanueva@users.noreply.github.com>
2024-05-13 15:25:30 -0300

    app.js

--- app.js
+++ app.js
@@ -270,7 +270,7 @@ if (diainiciado === "1" || diainiciado === "01") {
 //acusado
     pdf.setFont("Arial", "bold");
     pdf.setFontSize(15);
-    pdf.text(("ACUSADO/A:")+(". "), 56, 543);
+    pdf.text(("ACUSADO/A:")+(". a"), 56, 543);
     pdf.line(56, 546, 152, 546); // (x1, y1, x2, y2)
     pdf.setFont("Arial", "");
     pdf.setFont("Arial");
