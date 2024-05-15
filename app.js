77d56f7cc0595b108f904878db84a7c3007f861d
fdlfernandovillanueva <141683660+fdlfernandovillanueva@users.noreply.github.com>
2024-05-15 00:01:56 -0300

    app.js

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ if (diainiciado === "1" || diainiciado === "01") {
     }
     
     // Ejemplo de uso:
-    var texto = ((".      .        .     .         .      .       ")+lugarhecho.toUpperCase()+(" (VÍA PÚBLICA)"));
+    var texto = ((".      .       .      .         .      .       .      ")+lugarhecho.toUpperCase()+(" (VÍA PÚBLICA)"));
     var anchoMaximo = 38; // ajusta este valor según tu diseño
     var lineHeight = 20; // ajusta este valor según tu diseño
     var x = 56; // ajusta esta coordenada según tu diseño
